b0baaf917916b46412e95a7c13c65e23d246155b
aldocarrolo <aldo.carrolo@gmail.com>
2018-12-18 15:03:59 +0100

    Fixed join room function (in app.js)

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var serv = require('http').Server(app);
 
+var rooms = [];
+
 app.get('/',function(req, res) {
     res.sendFile(__dirname + '/client/index.html');
 });
@@ -14,21 +16,45 @@ console.log("Server started.");
 var io = require('socket.io')(serv);
 io.sockets.on('connection', function(socket){
 
-    console.log('Client Connected');
-    
     socket.on('New room', function() {
-        console.log('New Room')
-        r = new Room();     
+
+        console.log('Client create new room...')
+        r = new Room();
         player = new Player(socket, "Dany");
         r.join(player);
-        console.log(r);
-        socket.join(r.code);   
+
+        rooms.push(r);
+        console.log(rooms);
+        socket.join(r.code);
     });
 
     socket.on('Join room', function(code) {
-        console.log('Join ' + code.code);
+        console.log('Client join in room with code ' + code.code + '...');
+
+        var player = new Player(socket, "Mokka");
+
+        for(r in rooms) {
+
+            console.log(code + " " + rooms[r].code);
+
+            if(code.code == rooms[r].code) {
+
+                if(rooms[r].join(player)) {
+
+                    console.log(rooms[r]);
+                    socket.join(rooms[r].code);
+
+                } else {
+
+                    console.log("Room is full!");
+                }
+            } else {
+
+                console.log("Room doesn't exist");
+            }
+        }
     });
-    
+
 });
 
 
@@ -47,19 +73,25 @@ function Room() {
     this.MAX_NUMBER_PLAYER = 10;
 
     this.isFull = function () {
+
         if(this.players.length >= this.MAX_NUMBER_PLAYER)
             return true;
         return false;
     }
 
     this.join = function(player) {
-        if(!this.isFull())
+
+        if(!this.isFull()) {
             this.players.push(player);
+            return true;
+        }
+
+        return false;
     }
 }
 
 function createCode() {
-        
+
     var text = "";
     var possible = "ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnpqrstuvwxyz";
 
